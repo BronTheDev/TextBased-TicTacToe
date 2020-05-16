606056a6337e79782c997cbf191b725958c77fbe
AubronM <bronthedev@gmail.com>
2020-05-16 10:08:22 -0400

    Update Project_One_TicTacToe.py

--- Project_One_TicTacToe.py
+++ Project_One_TicTacToe.py
@@ -1,26 +1,7 @@
 """
-Your assignment: Create a Tic Tac Toe game. You are free to use any IDE you like.
+Author: BronTheDev
 
-Here are the requirements:
-
-2 players should be able to play the game (both sitting at the same computer)
-The board should be printed out every time a player makes a move
-You should be able to accept input of the player position and then place a symbol on the board
-
-
-
-
-     |       |
-     |       |
--------------------
-     |       |
-     |       |
--------------------
-     |       |
-     |       |
-
-
-
+Simple Text Based TicTacToe Game
 """
 print("Welcome to TicTacToe \n")
 
